1013037b5d62def35ceea79e55701bdffafd635f
Josko Nakic <joskonakic@MacBook-Pro-od-Josko.local>
2020-06-14 20:30:24 +0200

    Updates

---
+++ precache-manifest.5beca8ee246d6482301fd8ff11ffce3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c6958feb434a8211c5e07f42615d52",
+    "url": "/react-holiday-lister/index.html"
+  },
+  {
+    "revision": "d69bee41fac056371866",
+    "url": "/react-holiday-lister/static/css/main.12f55f88.chunk.css"
+  },
+  {
+    "revision": "2cadb83869593deef876",
+    "url": "/react-holiday-lister/static/js/2.74c1eeb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-holiday-lister/static/js/2.74c1eeb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69bee41fac056371866",
+    "url": "/react-holiday-lister/static/js/main.ddcc95c1.chunk.js"
+  },
+  {
+    "revision": "e425ca0b6c635cd0aa66",
+    "url": "/react-holiday-lister/static/js/runtime-main.05c2fc07.js"
+  }
+]);
